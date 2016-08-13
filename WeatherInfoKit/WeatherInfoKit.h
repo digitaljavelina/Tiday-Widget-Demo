1858dbe5f3e11db3121b53b1b2034b072192c209
Michael Henry <mike@digitaljavelina.com>
2016-08-12 23:16:47 -0700

    initial commit
    Signed-off-by: Michael Henry <mike@digitaljavelina.com>

---
+++ WeatherInfoKit/WeatherInfoKit.h
@@ -0,0 +1,19 @@
+//
+//  WeatherInfoKit.h
+//  WeatherInfoKit
+//
+//  Created by Michael Henry on 12/22/15.
+//  Copyright © 2015 AppCoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WeatherInfoKit.
+FOUNDATION_EXPORT double WeatherInfoKitVersionNumber;
+
+//! Project version string for WeatherInfoKit.
+FOUNDATION_EXPORT const unsigned char WeatherInfoKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherInfoKit/PublicHeader.h>
+
+
